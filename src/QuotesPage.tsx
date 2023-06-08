1c1e174d1f4bdecc8de1a0e89c2e90c0e6ccb92c
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-06-08 12:05:45 +0200

    Quotes page styling

--- src/QuotesPage.tsx
+++ src/QuotesPage.tsx
@@ -2,6 +2,11 @@ import React, {ReactNode, useEffect} from 'react';
 import { useAppDispatch, useAppSelector } from './stores/hooks';
 import { fetchData } from './stores/quotesSlice';
 import { Quote } from './interfaces/Quote';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { FormatQuote } from '@material-ui/icons';
+
+import './Quotes.css'
 
 
 const QuotesPage: React.FC = () => {
@@ -23,17 +28,23 @@ const QuotesPage: React.FC = () => {
 
   // Add your component's logic and JSX structure here
 
-  let quoteList : ReactNode[] = []
 
-  data?.quotes.forEach((quote, index) => {
-    quoteList.push(<li key={index}> {quote.quote} </li>)
-  })
 
 
     return (
-        <div>
-           {data?.limit}
-           {quoteList}
+        <div className='quotes-container'>
+          <Carousel>
+          {data?.quotes.map((item, index) => (
+          <div className='quote' key={index}>
+            <FormatQuote className='quote-icon'/>
+            <div className='quote-content'>
+          <h1 className='quote-header'>{item.quote}</h1>
+          <p className='quote-author'>{item.author}</p>
+          </div>
+            
+            </div>
+        ))}
+           </Carousel>
         </div>
       );
   
